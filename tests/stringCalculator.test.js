01b5802122afcab921f5ddcfa9b38117d90c29af
devsachin007 <23sachin04@gmail.com>
2024-10-15 22:23:50 +0530

    test case 9 for handling delimiters of any length

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -41,4 +41,9 @@ describe('add function', () => {
         expect(add("//[***]\n1***2***3")).toBe(6);
     });
 
+    //test 9
+    it('should support multiple delimiters of any length', () => {
+        expect(add("//[***][%]\n1***2%3")).toBe(6);
+    });
+
 });
